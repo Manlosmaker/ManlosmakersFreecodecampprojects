6b3cf877f811ca3a74fc4ffbd9a2ee5237fe128f
manlos <gabrielrks@icloud.com>
2025-06-16 22:42:35 +0100

    this is definetly the sketchiest code I have every written

--- arithmeticformmater.py
+++ arithmeticformmater.py
@@ -12,12 +12,28 @@ def arithmetic_arrange(calculations, answer = False):
         operator.append(splitItem[1])
     
     for i in range(len(calculations)):
-        individialLine = max(len(firstOperand[i]), len(secondOperand[i]) + 1) * " " + firstOperand[i]
-        firstOperandLine = firstOperandLine + individialLine + "    "
-        secondindividualLine = operator[i] + (len(firstOperand[i]) * " ") + secondOperand[i]
-        secondOperandLine = secondOperandLine + secondindividualLine + "    "
+        def miL(): 
+            if len(firstOperand[i]) >= len(secondOperand[i]):
+                individialLine = 2 * " " + firstOperand[i]
+            else:
+                individialLine = (len(secondOperand[i]) - len(firstOperand[i]) + 2) * " " + firstOperand[i]
+            return individialLine
+        firstOperandLine = firstOperandLine + miL() + "    "
+        def siL():
+            if len(secondOperand[i]) >= len(firstOperand[i]): 
+                secondindividualLine = operator[i] + " " + secondOperand[i]
+            elif len(firstOperand[i]) == 3: 
+                secondindividualLine = operator[i] + 2 * " " + secondOperand[i]
+            else: 
+                secondindividualLine = operator[i] + (len(firstOperand[i])) * " " + secondOperand[i]
+
+            return secondindividualLine
+        
+        secondOperandLine = secondOperandLine + siL() + "    "
     print(firstOperandLine)
     print(secondOperandLine)
+    print(("-" * (max(len(secondOperand[i]), len(firstOperand[i])) + 2) + 4 * " ") * len(calculations))
+
 
 calculations = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
 arithmetic_arrange(calculations)
