38912bb0460e7e8e330047826e059dc75e8834f6
manlos <gabrielrks@icloud.com>
2025-06-17 20:43:54 +0100

    fixed an issue where if the numbers added together to add another column would be out of sync

--- arithmeticformmater.py
+++ arithmeticformmater.py
@@ -47,8 +47,10 @@ def arithmetic_arranger(problems, show_answers = False):
     print(firstOperandLine)
     print(secondOperandLine)
     print(thirdindividualLine)
-    print(fourthindividualline)
+    if fourthindividualline != "":
+        print(fourthindividualline)
+    else:
+        pass
 
 
-
-arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+#arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"])
