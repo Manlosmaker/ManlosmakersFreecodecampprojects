a0f961383ecd0086dea27f3262d42c86ef9ba958
manlos <gabrielrks@icloud.com>
2025-06-20 19:27:11 +0100

    Updating to fix with freecodecamp errors

--- arithmeticformatter.py
+++ arithmeticformatter.py
@@ -1,4 +1,12 @@
-def arithmetic_arranger(problems, show_answers=False):
+def arithmetic_arranger(problems, show_answers=True):
+    line1 = ""
+    line2 = ""
+    line3 = ""
+    line4 = ""
+
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    
     for item in problems:
         #splitting items
         splitItem = item.split(" ")
@@ -7,12 +15,31 @@ def arithmetic_arranger(problems, show_answers=False):
         num2 = int(splitItem[2])
         maxLen = max(len(str(num1)),len(str(num2)))
 
-        #line 1
-        line1 = maxLen - len(str(num1))
-        if maxLen - len(str(num1)) == 0:
-            line1 = f"{num1:>maxLen}"
-        print(line1)
+        #error handling
+        if operand != "+" and operand != "-":
+            return "Error: Operator must be '+' or '-'."
+        if type(num1) != int or type(num2) is not int:
+            return 'Error: Numbers must only contain digits.'
+        if len(str(num1)) > 4 or len(str(num2)) > 4:
+            return 'Error: Numbers cannot be more than four digits.'
+        #making the lines
+        line1 = line1 + f"{num1:>{maxLen + 2}}    " 
+        line2 = line2 + f"{operand} {str(num2):>{maxLen}}    "
+        line3 = line3 + "-" * (maxLen + 2) + "    "  
+        #checking if show answers is true
+        if show_answers and operand == "+":
 
+            line4 = line4 + f"{(num1 + num2):>{maxLen+2}}    "
+        elif show_answers and operand == "-":
+            line4 = line4 + f"{(num1 - num2):>{maxLen+2}}    "
+    lines = [
+        line1.rstrip(),
+        line2.rstrip(),
+        line3.rstrip(),
+    ]
+    if show_answers:
+        lines.append(line4.rstrip())
+
+    problems = "\n".join(lines)
     return problems
-
 print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
